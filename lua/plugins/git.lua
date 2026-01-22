36366696138ae61d6a4b2df2d9d5f74c2c123015
Stanislav Atanasov <stanislav.atan@gmail.com>
2026-01-22 13:14:56 -0500

    [feat] add gitsigns toggle and prevent key collisions

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -11,10 +11,38 @@ return {
     config = function()
       require("gitsigns").setup({
         -- Show "Git Blame" information on the current line (like GitLens)
-        current_line_blame = true,
+        current_line_blame = false, -- Default to off (toggle with <leader>gb)
         current_line_blame_opts = {
           delay = 500, -- Delay in ms before blame appears
         },
+        on_attach = function(bufnr)
+          local gs = package.loaded.gitsigns
+
+          local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+          end
+
+          -- Navigation (next/prev hunk) - using c (change) to avoid collision with CoC's g (diagnostics)
+          map("n", "]c", function()
+            if vim.wo.diff then return "]c" end
+            vim.schedule(function() gs.next_hunk() end)
+            return "<Ignore>"
+          end, { expr = true })
+
+          map("n", "[c", function()
+            if vim.wo.diff then return "[c" end
+            vim.schedule(function() gs.prev_hunk() end)
+            return "<Ignore>"
+          end, { expr = true })
+
+          -- Actions
+          map("n", "<leader>gb", gs.toggle_current_line_blame, { desc = "Toggle Git Blame" })
+          map("n", "<leader>gd", gs.preview_hunk, { desc = "Preview Git Hunk" })
+          map("n", "<leader>gr", gs.reset_hunk, { desc = "Reset Git Hunk" })
+          map("n", "<leader>gs", gs.stage_hunk, { desc = "Stage Git Hunk" })
+        end,
       })
     end
   },

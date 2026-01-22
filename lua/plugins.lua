7693bb0e989c31ffff328dca5badf3e1caccb394
Stanislav Atanasov <stanislav.atan@gmail.com>
2026-01-22 12:30:52 -0500

    [feat] add gitsigns for git-lens style blame and diffs

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -16,6 +16,17 @@ require("lazy").setup({
   { "honza/vim-snippets" },
   { "tpope/vim-fugitive" },
   { "kdheepak/lazygit.nvim" },
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup({
+        current_line_blame = true, -- Show blame on current line (like GitLens)
+        current_line_blame_opts = {
+          delay = 500,
+        },
+      })
+    end
+  },
   { "christoomey/vim-tmux-navigator" },
 
   -- Syntax & Treesitter
